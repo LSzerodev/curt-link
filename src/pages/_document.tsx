1cbc6cb53206c63e5ef15ff6e07c4b038fa4dfdc
kkkengranado <[luisfelipe92088@gmail.com]>
2025-08-04 19:44:20 -0300

    Adicionei ads no _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,16 @@
+// pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-BR"> {/* Altere para português se seu público é brasileiro */}
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2589071339817154"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
